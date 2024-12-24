4138a0c6a62560389ec36e45d779ddaf2d96ec04
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2024-12-24 10:47:41 +0900

    change"NavBar"

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -12,24 +12,60 @@ const NavBar = () => {
   }, [isAuthenticated]);
 
   return (
-    <div>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
-      )}
-
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-      {isAuthenticated && (
-        <div className="nav-bar">
-          <Link to="/">Home</Link>&nbsp;
-          <Link to="/profile">Profile</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Your Brand
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {isAuthenticated && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+          <div className="d-flex">
+            {/* {!isAuthenticated && (
+              <button
+                className="btn btn-outline-light"
+                onClick={() => loginWithRedirect({})}
+              >
+                Log in
+              </button>
+            )} */}
+            {isAuthenticated && (
+              <button
+                className="btn btn-outline-light"
+                onClick={() => logout()}
+              >
+                Log out
+              </button>
+            )}
+          </div>
         </div>
-
-        // <span>
-        //   <Link to="/">Home</Link>&nbsp;
-        //   <Link to="/profile">Profile</Link>
-        // </span>
-      )}
-    </div>
+      </div>
+    </nav>
   );
 };
 
